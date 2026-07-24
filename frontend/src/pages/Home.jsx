acb9792e4076f9131fd214d435436462b16d4230
sruthi939 <sathiyasuru@gmail.com>
2026-07-24 15:08:03 +0530

    add AppDownload component and integrate it into the Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Header from '../components/Header'
 import ExploreMenu from '../components/ExploreMenu'
 import FoodDisplay from '../components/FoodDisplay';
+import AppDownload from '../components/AppDownload';
 
 const Home = () => {
     const [category, setCategory] = useState("All");
@@ -13,6 +14,7 @@ const Home = () => {
                 <ExploreMenu category={category} setCategory={setCategory} />
             </div>
             <FoodDisplay category={category} />
+            <AppDownload />
         </div>
     )
 }
