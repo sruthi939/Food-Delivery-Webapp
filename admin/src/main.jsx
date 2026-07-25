bb531b311487e1e7b702581e3c585f5777b85939
sruthi939 <sathiyasuru@gmail.com>
2026-07-26 01:24:49 +0530

    initialize admin dashboard entry point with routing and context providers

---
+++ admin/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { AdminContextProvider } from './context/AdminContext.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <AdminContextProvider>
+                <App />
+            </AdminContextProvider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
